ecaea1928c2a2366aeafe6561c48870ed6baf958
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-05-26 09:09:01 +0300

    ЧТООООООООООООО

--- lab7/all.pl
+++ lab7/all.pl
@@ -53,3 +53,64 @@ isFloatIntAlternate([H|T],Flag,NowFlag):-
     ).
     
 task2:- read(N),readList(List,N),isFloatIntAlternate(List,Flag),write(Flag),!.
+%3 Дан список. Построить массив из элементов, делящихся на свой номер и встречающихся в исходном массиве 1 раз.
+isElemUnique(List,Elem):-countOfThisElem(List,Elem,Count),1 is Count.
+
+countOfThisElem([],Elem,Count):- Count is 0,!.
+countOfThisElem([H|T],Elem,Count):-
+    H = Elem,
+
+    countOfThisElem(T,Elem,Count1),
+    Count is 1+Count1;
+
+    countOfThisElem(T,Elem,Count),!.   
+
+listUniqueWithIndex(List,UniList,UniIndexList):-listUniqueWithIndex(List,UniList,UniIndexList,0,List).
+listUniqueWithIndex([],UniList,UniIndexList,Index,StartList):-makeEmptyList(UniList),makeEmptyList(UniIndexList),!.
+listUniqueWithIndex([H|T],UniList,UniIndexList,Index,StartList):-
+    I1 is Index+1,
+    (
+        isElemUnique(StartList,H),
+
+        listUniqueWithIndex(T,UniList1,UniIndexList1,I1,StartList),
+        append([H],UniList1,UniList),
+        append([I1],UniIndexList1,UniIndexList);
+
+        listUniqueWithIndex(T,UniList,UniIndexList,I1,StartList)
+    ),!.
+
+listDelOnAnotherList([],[],NewList).
+listDelOnAnotherList([UH|UT],[IH|IT],NewList):-
+    0 is UH mod IH,
+
+    listDelOnAnotherList(UT,IT,NewList1),
+    append([UH],NewList1,NewList),!;
+
+    listDelOnAnotherList(UT,IT,NewList),!.
+
+listDelOnIndexAndUnique(List,NewList):-
+    listUniqueWithIndex(List,UniList,UniIndexList),
+    listDelOnAnotherList(UniList,UniIndexList,NewList).
+
+task3:- read(N),readList(List,N),listDelOnIndexAndUnique(List,NewList),writeList(NewList),!.
+%4 Беседует трое друзей: Белокуров, Рыжов, Чернов. Брюнет
+% сказал Белокурову: “Любопытно, что один из нас блондин, другой брюнет,
+% третий - рыжий, но ни у кого цвет волос не соответствует фамилии”. Какой
+% цвет волос у каждого из друзей?
+inList([],_):-fail.
+inList([X|_],X).
+inList([_|T],X):-inList(T,X).
+
+task4:- 
+    Hairs=[_,_,_],
+    inList(Hairs,[belov,_]),
+    inList(Hairs,[chernov,_]),
+    inList(Hairs,[rijov,_]),
+    inList(Hairs,[_,rij]),
+    inList(Hairs,[_,blond]),
+    inList(Hairs,[_,brunet]),
+    not(inList(Hairs,[belov,blond])),
+    not(inList(Hairs,[chernov,brunet])),
+    not(inList(Hairs,[rijov,rij])),
+    write(Hairs),
+    !.
